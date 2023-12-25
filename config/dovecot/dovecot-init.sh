af33376af1cb67836f2800ac45932c5a59268189
francois <francois@mahoneko.net>
2023-12-25 14:51:36 +0100

    update

--- config/dovecot/dovecot-init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/ash
-
-sed -i "$ a ssl_cert=</etc/letsencrypt/live/${HOSTNAME}/fullchain.pem" /etc/dovecot/dovecot.conf
-sed -i "$ a ssl_key=</etc/letsencrypt/live/${HOSTNAME}/privkey.pem" /etc/dovecot/dovecot.conf
-
-/usr/sbin/dovecot -F
--- config/postfix/postfix-init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-/usr/sbin/postconf "myhostname = ${HOSTNAME}"
-/usr/sbin/postconf "smtpd_tls_cert_file = ${SSL_CERT_PUBLIC}"
-/usr/sbin/postconf "smtpd_tls_key_file = ${SSL_CERT_PRIVATE}"
-
-/usr/sbin/postfix start-fg
-
